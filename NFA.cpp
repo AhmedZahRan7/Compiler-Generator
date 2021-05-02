e2f1a2cb15c559a8ff216c126cc98e5a355356ec
AhmedZahRan7 <ahmed.mimo720@gmail.com>
2021-05-02 23:01:13 +0200

    remove redundent code in nfa operations functions

--- NFA.cpp
+++ NFA.cpp
@@ -5,19 +5,18 @@ NFA::NFA(){
 }
 
 NFA::NFA(string input){
-    this->states.insert(this->startState = new State());
-    this->states.insert(this->finalState = new State());
-    this->transationsFromState[this->startState].insert(
-        new Transation(this->startState,this->finalState,input));
+    addState(this->startState = new State());
+    addState(this->finalState = new State());
+    addTransation(this->startState,this->finalState,input);
     this->finalState->markAsAcceptingState();
 }
 
 NFA* NFA::concatinateWith(NFA* secondNFA){
-    this->transationsFromState[this->finalState].insert(
-        new Transation(this->finalState,secondNFA->startState,EPSILON_TRANSATION));
-    for(State* state : secondNFA->states) this->states.insert(state);
+    addTransation(this->finalState,secondNFA->startState,EPSILON_TRANSATION);
+    for(State* state : secondNFA->states) addState(state);
     for(auto p : secondNFA->transationsFromState){
-        for(Transation* trans : p.second) this->transationsFromState[p.first].insert(trans);
+        for(Transation* trans : p.second)
+            addTransation(trans->from,trans->to,trans->condition);
     }
     this->finalState->markAsNotAcceptingState();
     this->finalState = secondNFA->finalState;
@@ -28,15 +27,16 @@ NFA* NFA::unionWith(NFA* secondNFA){
     State *newStart = new State();
     State *newfinal = new State();
     newfinal->markAsAcceptingState();
-    this->transationsFromState[newStart].insert(new Transation(newStart,this->startState,EPSILON_TRANSATION));
-    this->transationsFromState[newStart].insert(new Transation(newStart,secondNFA->startState,EPSILON_TRANSATION));
-    this->transationsFromState[this->finalState].insert(new Transation(this->finalState,newfinal,EPSILON_TRANSATION));
-    this->transationsFromState[secondNFA->finalState].insert(new Transation(secondNFA->finalState,newfinal,EPSILON_TRANSATION));
-    this->states.insert(newStart);
-    this->states.insert(newfinal);
-    for(State* state : secondNFA->states) this->states.insert(state);
+    addTransation(newStart,this->startState,EPSILON_TRANSATION);
+    addTransation(newStart,secondNFA->startState,EPSILON_TRANSATION);
+    addTransation(this->finalState,newfinal,EPSILON_TRANSATION);
+    addTransation(secondNFA->finalState,newfinal,EPSILON_TRANSATION);
+    addState(newStart);
+    addState(newfinal);
+    for(State* state : secondNFA->states) addState(state);
     for(auto p : secondNFA->transationsFromState){
-        for(Transation* trans : p.second) this->transationsFromState[p.first].insert(trans);
+        for(Transation* trans : p.second)
+            addTransation(trans->from,trans->to,trans->condition);
     }
     this->finalState->markAsNotAcceptingState();
     this->finalState = newfinal;
@@ -48,12 +48,12 @@ NFA* NFA::convertIntoKleeneclosure(){
     State *newStart = new State();
     State *newfinal = new State();
     newfinal->markAsAcceptingState();
-    this->transationsFromState[this->finalState].insert(new Transation(this->finalState,this->startState,EPSILON_TRANSATION));
-    this->transationsFromState[newStart].insert(new Transation(newStart,this->startState,EPSILON_TRANSATION));
-    this->transationsFromState[newStart].insert(new Transation(newStart,newfinal,EPSILON_TRANSATION));
-    this->transationsFromState[this->finalState].insert(new Transation(this->finalState,newfinal,EPSILON_TRANSATION));
-    this->states.insert(newStart);
-    this->states.insert(newfinal);
+    addTransation(this->finalState,this->startState,EPSILON_TRANSATION);
+    addTransation(this->finalState,newfinal,EPSILON_TRANSATION);
+    addTransation(newStart,this->startState,EPSILON_TRANSATION);
+    addTransation(newStart,newfinal,EPSILON_TRANSATION);
+    addState(newStart);
+    addState(newfinal);
     this->finalState->markAsNotAcceptingState();
     this->finalState = newfinal;
     this->startState = newStart;
@@ -64,11 +64,11 @@ NFA* NFA::convertIntoPostiveclosure(){
     State *newStart = new State();
     State *newfinal = new State();
     newfinal->markAsAcceptingState();
-    this->transationsFromState[this->finalState].insert(new Transation(this->finalState,this->startState,EPSILON_TRANSATION));
-    this->transationsFromState[newStart].insert(new Transation(newStart,this->startState,EPSILON_TRANSATION));
-    this->transationsFromState[this->finalState].insert(new Transation(this->finalState,newfinal,EPSILON_TRANSATION));
-    this->states.insert(newStart);
-    this->states.insert(newfinal);
+    addTransation(this->finalState,this->startState,EPSILON_TRANSATION);
+    addTransation(this->finalState,newfinal,EPSILON_TRANSATION);
+    addTransation(newStart,this->startState,EPSILON_TRANSATION);
+    addState(newStart);
+    addState(newfinal);
     this->finalState->markAsNotAcceptingState();
     this->finalState = newfinal;
     this->startState = newStart;
@@ -92,10 +92,6 @@ void NFA::addState(State* state){
 void NFA::addTransation(State* from,State* to,string cond){
     this->transationsFromState[from].insert(new Transation(from,to,cond));
 }
-
-/**
- * @todo use addState() and addTrans() in other implementations 
- */
 
 set<State*> NFA::getStates(){
     return this->states;
